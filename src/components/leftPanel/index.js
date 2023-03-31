f90892bf720339928bd424e51553131464579d9c
veekays <vikaskumarsingh9838@gmail.com>
2023-04-01 02:27:42 +0530

    chore: search implementation on left nav

--- src/components/leftPanel/index.js
+++ src/components/leftPanel/index.js
@@ -1,28 +1,53 @@
 import React from "react";
 import navData from "./navData";
-import { VerticalNav } from "@innovaccer/design-system";
+import { VerticalNav, Input } from "@innovaccer/design-system";
 
 export const LeftPanel = () => {
   const [active, setActive] = React.useState({
     name: "Avatar",
   });
 
+  const [data, setData] = React.useState(navData);
+
   const onClickHandler = (menu) => {
-    console.log("menu-clicked: ", menu);
     setActive(menu);
   };
+
+  const [value, setValue] = React.useState("");
+
+  const onChange = React.useCallback((e) => {
+    let filteredData = navData.filter((item) =>
+      item.label.toLocaleLowerCase().includes(e.target.value)
+    );
+    setValue(e.target.value);
+    setData(filteredData)
+  }, []);
+
+  const onClear = React.useCallback(() => {
+    setValue("");
+    setData(navData);
+  }, []);
+
   return (
-    <div
-      data-test="Docs-Leftnav"
-      id="navbar-container"
-      class="h-80vh bg-secondary-lightest border-right page-scroll"
-    >
-      <VerticalNav
-        menus={navData}
-        active={active}
-        onClick={onClickHandler}
-        hoverable={false}
+    <div>
+      <Input
+        name="input"
+        onChange={onChange}
+        onClear={onClear}
+        placeholder="Search component"
+        value={value}
+        size="large"
+        className="mb-3"
+        icon="search"
       />
+      <div className="vertical-nav-container bg-secondary-lightest page-scroll">
+        <VerticalNav
+          menus={data}
+          active={active}
+          onClick={onClickHandler}
+          hoverable={false}
+        />
+      </div>
     </div>
   );
 };
