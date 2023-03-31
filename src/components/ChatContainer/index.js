b283adb4bc519c3d7271a58ccb1025ca7aabfd73
veekays <vikaskumarsingh9838@gmail.com>
2023-04-01 00:31:51 +0530

    add right panel to container

--- src/components/ChatContainer/index.js
+++ src/components/ChatContainer/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import LeftPanel from "../leftPanel";
+import rightPanel from "../rightPanel";
 
 export const ChatContainer = () => {
   return <div className="App-container">hello world</div>;
---
+++ src/components/rightPanel/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const rightPanel = () => {
+    return <div>right panel</div>
+}
+
+export default rightPanel;
